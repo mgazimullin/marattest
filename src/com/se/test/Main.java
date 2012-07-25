038cbfc41470b66822f940c64296e622a0434934
Marat Gazimullin <marat.gazimullin@smartengine.at>
2012-07-25 14:28:31 +0200

    some changes

---
+++ src/com/se/test/Main.java
@@ -0,0 +1,13 @@
+package com.se.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO: short description
+ *
+ * @author m.gazimullin
+ */
+public class Main {
+	private final static Logger log = LoggerFactory.getLogger(Main.class);
+}
