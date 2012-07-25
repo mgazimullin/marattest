72c3be4e520919f3457ccc87bb41d3f715bf88a9
Marat Gazimullin <marat.gazimullin@smartengine.at>
2012-07-25 16:04:48 +0200

    123

--- src/com/se/test/Main.java
+++ src/com/se/test/Main.java
@@ -14,6 +14,6 @@ public class Main {
 	private final static Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) {
-		log.info("test1");
+		log.info("test12");
 	}
 }
